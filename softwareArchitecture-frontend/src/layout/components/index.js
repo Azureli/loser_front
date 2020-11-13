bf909c619f48c55e6419d448e97c2737f49fcab6
897811139@qq.com <31436943+Azureli@users.noreply.github.com>
2020-11-13 22:18:40 +0800

    login register

--- softwareArchitecture-frontend/src/layout/components/index.js
+++ softwareArchitecture-frontend/src/layout/components/index.js
@@ -1,4 +1,4 @@
 export { default as AppMain } from './AppMain'
 export { default as Topbar } from './Topbar/index.vue'
 export { default as Logo } from './Logo.vue'
-export { default as Navbar } from './NavBar.vue'
+export { default as Navbar } from './Navbar.vue'
