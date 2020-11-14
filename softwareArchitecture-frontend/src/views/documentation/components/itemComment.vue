27aa6a6f8343d029322cb94d40df3ef95e469e17
897811139@qq.com <31436943+Azureli@users.noreply.github.com>
2020-11-14 17:05:39 +0800

    documentation

---
+++ softwareArchitecture-frontend/src/views/documentation/components/itemComment.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="div-comment">
+    <p>{{commentInfo.stuNum}}</p>
+    <p>{{commentInfo.comment}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ItemComment",
+  props: {
+    commentInfo: {
+      type: Object,
+      default: () => {
+        return {
+          stuNum:"1120162047",
+          comment:"整挺好真不错"
+        };
+      },
+    },
+  },
+  data() {
+    return {};
+  },
+  methods: {},
+};
+</script>
+
+<style lang="scss" scope>
+.div-comment{
+  background-color: rgba(222,222,222,0.2);
+  padding:1%;
+  border: 1px solid;
+  border-color: rgba(222,222,222,0.2);
+  
+}
+</style>
+
--- softwareArchitecture-frontend/src/views/documentation/index.vue
+++ softwareArchitecture-frontend/src/views/documentation/index.vue
@@ -1,60 +1,91 @@
 <template>
-
-<el-row  class="dish-container">
-  <el-col :span="8"  class="grid-content">
-    <div class="dish-image">
-      <el-image :src="src"></el-image>
-    </div>
-  </el-col>
-  <el-col :span="12" :offset="2" class="grid-content">
-      <p class="title" >{{dish}}  
-        <el-link  href="#/dashboard" class="link">返回首页</el-link></p>
+  <el-row class="dish-container">
+    <el-col :span="8">
+      <div class="dish-image">
+        <el-image :src="src"></el-image>
+      </div>
+    </el-col>
+    <el-col :span="12" :offset="2">
+      <p class="title">
+        {{dish}}
+        <el-link href="#/dashboard" class="link">返回首页</el-link>
+      </p>
       <p class="dishprice">￥{{dishPrice}}</p>
 
       <h5>主要原料</h5>
-      <p>        {{material}}      </p>
+      <p>{{material}}</p>
       <h5>其他说明</h5>
-      <p>        {{explanations}}  <el-button class="dark-red-btn" :loading="loading">现在预订</el-button>    </p>
-      
+      <p>
+        {{explanations}}
+        <el-button class="dark-red-btn">现在预订</el-button>
+      </p>
     </el-col>
-</el-row>
-
+    <el-col :span="22" class="comment">
+      <h5>用户评论</h5>
+      <item-comment v-for="(i, ind) in itemList" :key="ind" :commentInfo="i" :style="randomRgb(i)" class="line-style" />
+    </el-col>
+  </el-row>
 </template>
 
 <script>
-import DropdownMenu from '@/components/Share/DropdownMenu'
-
+import DropdownMenu from "@/components/Share/DropdownMenu";
+import ItemComment from "./components/itemComment.vue";
+import { mount } from "@vue/test-utils";
 export default {
-  name: 'Documentation',
-  components: { DropdownMenu },
+  name: "Documentation",
+  components: { DropdownMenu, ItemComment },
   data() {
     return {
-       dish:"bug大餐",
-       dishPrice:"14",
-       material:"html,vue,宽油,耗子尾汁",
-       explanations:"吃了这道菜,不写bug,每天闪电五连鞭",
-       src: 'https://cube.elemecdn.com/6/94/4d3ea53c084bad6931a56d5158a48jpeg.jpeg',
-      articleList: [
-        { title: '基础篇', href: 'https://juejin.im/post/59097cd7a22b9d0065fb61d2' },
-        { title: '登录权限篇', href: 'https://juejin.im/post/591aa14f570c35006961acac' },
-        { title: '实战篇', href: 'https://juejin.im/post/593121aa0ce4630057f70d35' },
-        { title: 'vue-admin-template 篇', href: 'https://juejin.im/post/595b4d776fb9a06bbe7dba56' },
-        { title: 'v4.0 篇', href: 'https://juejin.im/post/5c92ff94f265da6128275a85' },
-        { title: '自行封装 component', href: 'https://segmentfault.com/a/1190000009090836' },
-        { title: '优雅的使用 icon', href: 'https://juejin.im/post/59bb864b5188257e7a427c09' },
-        { title: 'webpack4（上）', href: 'https://juejin.im/post/59bb864b5188257e7a427c09' },
-        { title: 'webpack4（下）', href: 'https://juejin.im/post/5b5d6d6f6fb9a04fea58aabc' }
-      ]
+      dish: "bug大餐",
+      dishPrice: "14",
+      material: "html,vue,宽油,耗子尾汁",
+      explanations: "吃了这道菜,不写bug,每天闪电五连鞭",
+      src:
+        "https://cube.elemecdn.com/6/94/4d3ea53c084bad6931a56d5158a48jpeg.jpeg",
+      itemList: []
+    };
+  },
+  methods: {
+    randomRgb(item) {
+      let R = Math.floor(Math.random() * 130+100);
+      let G = Math.floor(Math.random() * 130+100);
+      let B = Math.floor(Math.random() * 130+100);
+      return {
+        background: 'rgba(' + R + ',' + G + ',' + B + ',0.1)'
+        };
+    },
+    getList() {
+      this.itemList = [
+        {
+          stuNum: "1120162047",
+          comment: "整挺好真不错"
+        },
+        {
+          stuNum: "1120168888",
+          comment: "贵了"
+        },
+        {
+          stuNum: "1120168888",
+          comment: "下次还来"
+        },
+        {
+          stuNum: "1125252566",
+          comment: "兄弟萌冲"
+        }
+      ];
     }
+  },
+  mounted() {
+    this.getList();
   }
-}
+};
 </script>
 
 <style lang="scss" scope>
-.dish-container{
+.dish-container {
   text-align: left;
-  margin:5% 10% 10% 15%;
-  .title{
+  margin: 5% 10% 10% 15%;
+  .title {
     font-size: 30px;
     display: block;
     margin-block-start: 0;
@@ -63,25 +94,26 @@ export default {
     margin-inline-end: 0px;
     font-weight: bold;
   }
-  .dishprice{
-    color:red;
+  .dishprice {
+    color: red;
   }
-  .dish-image{
-    
+  .dish-image {
   }
-  .h3{
+  .h3 {
     font-size: 20px;
   }
-  .link{
-    float:right;
-    color:grey;
+  .link {
+    float: right;
+    color: grey;
   }
-  .dishintro{
-    float:right;
+  .dishintro {
+    float: right;
   }
-  .el-button{
-    float:right;
+  .el-button {
+    float: right;
+  }
+  .comment{
+    
   }
 }
-
 </style>
