31fe5462311990650c8c12c034d190317f274479
897811139@qq.com <31436943+Azureli@users.noreply.github.com>
2021-01-01 16:58:23 +0800

    招聘职位简介

--- softwareArchitecture-frontend/src/views/documentation/index.vue
+++ softwareArchitecture-frontend/src/views/documentation/index.vue
@@ -1,34 +1,39 @@
 <template>
   <el-row class="dish-container">
-    <el-col :span="8">
-    <div >
-        <el-image :src="src"></el-image>
+    <div class="grey-head">
+      <h2>
+        {{position}}
+        <span class="dishprice">薪资 {{salary}}</span>
+         <el-button @click="submit-cv" v-permission="['user']">投递简历</el-button>
+      </h2>
+      <h3>
+        工作地点：{{workplace}}·招聘人数：{{Num}}·时间：{{worktime}}
+      </h3>
     </div>
-    </el-col>
-    <el-col :span="12" :offset="2">
-      <p class="title">
-        {{dish}}
+    <el-col :span="10" :offset="2">
+      <div>
+        <h2>
+          <i class="el-icon-caret-right"></i>工作内容
+        </h2>
+        <p>{{context}}</p>
+        <h2>
+          <i class="el-icon-caret-right"></i>岗位要求
+        </h2>
+        <p>{{pos_discrip}}</p>
         <el-link href="#/market" :underline="false" class="link">返回首页</el-link>
-      </p>
-      <p class="dishprice">￥{{dishPrice}}</p>
-
-      <h5>主要原料</h5>
-      <p>{{material}}</p>
-      <h5>其他说明</h5>
-      <p>
-        {{explanations}}
-        <el-button class="dark-red-btn" @click="orderdish" v-permission="['admin', 'user']">现在预订</el-button>
-      </p>
+      </div>
     </el-col>
-    <el-col :span="22" class="comment">
-      <h5>用户评论</h5>
-      <item-comment
-        v-for="(i, ind) in itemList"
-        :key="ind"
-        :commentInfo="i"
-        :style="randomRgb(i)"
-        class="line-style"
-      />
+    <el-col :span="8" :offset="2">
+      <el-card>
+        <el-image :src="src"></el-image>
+        <p class="title">{{company}}</p>
+        <h3>公司简介</h3>
+        <el-divider></el-divider>
+        <h5>岗位数量</h5>
+        <p>{{staffnum}}</p>
+        <h5>公司简介</h5>
+        <p>{{intro}}</p>
+      </el-card>
     </el-col>
   </el-row>
 </template>
@@ -36,28 +41,32 @@
 <script>
 import DropdownMenu from "@/components/Share/DropdownMenu";
 import ItemComment from "./components/itemComment.vue";
-import {viewDishDetail,viewComment,orderDish} from '@/api/myApis.js';
+import {} from "@/api/myApis.js";
 import { mapGetters } from "vuex";
 import permission from "@/directive/permission/index.js";
 
 export default {
   name: "Documentation",
-  components: { DropdownMenu, ItemComment },
+  components: {},
   directives: { permission },
   computed: {
-    ...mapGetters(["id"]),
+    ...mapGetters(["id"])
   },
   data() {
     return {
-      dishid:1,
-
-      dish: "bug大餐",
-      dishPrice: "14",
-      material: "html,vue,宽油,耗子尾汁",
-      explanations: "吃了这道菜,不写bug,每天闪电五连鞭",
-      src:
-        "F:\\图片\\61.bmp",
-      itemList: []
+      position: "顶级码农",
+      salary: "15k-20k",
+      workplace: "宿舍床上",
+      Num: "一个亿",
+      worktime: "996",
+      context:
+        "工作内容工作内容工作内容工作内容工作内容工作内容工作内容工作内容工作内容工作内容工作内容工作内容",
+      pos_discrip:
+        "睡大觉睡大觉睡大觉睡大觉睡大觉睡大觉睡大觉睡大觉睡大觉睡大觉睡大觉睡大觉睡大觉睡大觉",
+      company: "Microsoft",
+      staffnum: "1000",
+      intro: "每天九九六 快乐打工人",
+      src: "F:\\图片\\61.bmp"
     };
   },
   methods: {
@@ -70,49 +79,54 @@ export default {
       };
     },
     getList() {
-    let fd = new FormData();
-    fd.append('dishId',this.dishid);
-        viewComment(fd).then(res => {
-        console.log(res)
-        for (let i=0;i<res.comments.length;i++)
-        {
-          this.itemList.push(res.comments[i]);
-        }
-        }).catch(res => {
-            console.log(res)
+      let fd = new FormData();
+      fd.append("dishId", this.dishid);
+      viewComment(fd)
+        .then(res => {
+          console.log(res);
+          for (let i = 0; i < res.comments.length; i++) {
+            this.itemList.push(res.comments[i]);
+          }
         })
+        .catch(res => {
+          console.log(res);
+        });
     },
     getInfo() {
-    let fd = new FormData();
-    fd.append('id', this.dishid);
-      viewDishDetail(fd).then(res => {
-        console.log(res)
-        this.dish=res.data.dishName;
-        this.dishPrice=res.data.cost;
-        this.material=res.data.ingredient;
-        this.explanations=res.data.introduction;
-        this.src="http://127.0.0.1:8000/"+res.data.url;
-        }).catch(res => {
-            console.log(res)
+      let fd = new FormData();
+      fd.append("id", this.dishid);
+      viewDishDetail(fd)
+        .then(res => {
+          console.log(res);
+          this.dish = res.data.dishName;
+          this.dishPrice = res.data.cost;
+          this.material = res.data.ingredient;
+          this.explanations = res.data.introduction;
+          this.src = "http://127.0.0.1:8000/" + res.data.url;
         })
+        .catch(res => {
+          console.log(res);
+        });
     },
-    orderdish(){
-    let fd = new FormData();
-    fd.append('dishId',this.dishid);
-    fd.append('userId',this.id);
-    orderDish(fd).then(res => {
-    console.log(res)
-    }).catch(res => {
-        console.log(res)
-    })
-    this.$router.push({path:'/guide'})
+    orderdish() {
+      let fd = new FormData();
+      fd.append("dishId", this.dishid);
+      fd.append("userId", this.id);
+      orderDish(fd)
+        .then(res => {
+          console.log(res);
+        })
+        .catch(res => {
+          console.log(res);
+        });
+      this.$router.push({ path: "/guide" });
     }
   },
   mounted() {
     this.dishid = this.$route.query.dishId;
     this.getInfo();
     this.getList();
-    console.log(this.$route.query)
+    console.log(this.$route.query);
   }
 };
 </script>
@@ -120,27 +134,56 @@ export default {
 <style lang="scss" scope>
 .dish-container {
   text-align: left;
-  margin: 5% 10% 10% 15%;
+  margin: 0% 0% 0% 0%;
+  padding: 0;
+  .el-icon-caret-right {
+    color: rgb(255, 188, 0);
+  }
+  .el-card {
+    margin: 5%;
+    padding: 2%;
+    text-align: center;
+  }
   .el-link.el-link--default:hover {
     color: rgb(81, 15, 15) !important;
   }
+  .grey-head {
+    background-color: rgba(204, 204, 204);
+    padding: 1.5% 1.5% 1.5% 3%;
+    color: white;
+    .el-button {
+      width: 240px;
+      height: 50px;
+      background-color: rgb(255, 188, 0);
+      font-size: 20px;
+      color: white;
+      border: 0ch;
+      font-weight: bold;
+      margin-right: 5%;
+      margin-top: 0.5%;
+    }
+    .el-button :hover {
+      color: rgb(255, 234, 188);
+    }
+  }
   .title {
     font-size: 30px;
-    display: block;
     margin-block-start: 0;
     margin-block-end: 0;
     margin-inline-start: 0px;
     margin-inline-end: 0px;
     font-weight: bold;
+    object-position: right;
   }
   .dishprice {
     color: red;
   }
+
   .el-image {
-  width:380px;
-  height:300px;
-  object-fit: contain;
-object-position: top;
+    width: 200px;
+    height: 200px;
+    object-fit: contain;
+    object-position: top;
   }
   .h3 {
     font-size: 20px;
